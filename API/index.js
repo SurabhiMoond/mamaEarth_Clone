160637742be96ed5eb2e5ccfcd266182b233811e
Surabhi Moond <154314470+SurabhiMoond@users.noreply.github.com>
2024-07-02 15:44:44 +0530

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,10 +1,10 @@
-import jsonServer from "json-server";
+import jsonServer from 'json-server';
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log('JSON Server is running');
 });
