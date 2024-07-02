abd9bee281ff32b6118344b350296544f41f5dd8
Surabhi Moond <154314470+SurabhiMoond@users.noreply.github.com>
2024-07-02 21:45:29 +0530

    Update jsonUrls.js

--- mamaEarth/src/allJsonUrl/jsonUrls.js
+++ mamaEarth/src/allJsonUrl/jsonUrls.js
@@ -1,4 +1,5 @@
 export const jsonUrls = {
+  json_url:"https://mama-earth-clone.onrender.com",
   products: "https://mama-earth-clone.onrender.com/products",
   roles: "https://mama-earth-clone.onrender.com/roles",
 };
