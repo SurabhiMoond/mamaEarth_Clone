eb0cb85a193dc305bd6cc4c4dd46d19b26a5882e
Surabhi Moond <154314470+SurabhiMoond@users.noreply.github.com>
2024-07-02 19:32:43 +0530

    Update index.js

--- API/index.js
+++ API/index.js
@@ -3,8 +3,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running on port 3000");
-});
+
+server.listen(port);
